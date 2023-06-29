cb54943a0b6a24b3a9fcd42520a1f30f1dfaa7ce
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-06-29 21:18:53 +0100

    New button for Upcoming movies page.

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
 import { getUpcomingMovies } from "../api/tmdb-api";
 
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
-
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
 
   useEffect(() => {
     getUpcomingMovies().then(movies => {
@@ -27,8 +18,8 @@ const UpcomingMoviesPage = (props) => {
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+      action={() => {
+        return <AddToPlaylistIcon />
       }}
     />
   );
