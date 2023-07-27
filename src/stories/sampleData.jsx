c4736906ecf65a5d5eba994e5f08a4a915fd65c4
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-07-27 22:39:41 +0100

    Add Person Card component

--- src/stories/sampleData.jsx
+++ src/stories/sampleData.jsx
@@ -1,5 +1,5 @@
 
-export default {
+const sampleMovie = {
     adult: false,
     backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
     belongs_to_collection: {
@@ -79,4 +79,86 @@ export default {
     vote_average: 7,
     vote_count: 9692,
   };
+  
+  const samplePerson =     {
+    "adult": false,
+    "gender": 2,
+    "id": 2963,
+    "known_for": [
+      {
+        "adult": false,
+        "backdrop_path": "/bNgqt819qpHcszjCzLCG5y16ldF.jpg",
+        "genre_ids": [
+          16,
+          12,
+          10751,
+          14,
+          35,
+          28
+        ],
+        "id": 49519,
+        "media_type": "movie",
+        "original_language": "en",
+        "original_title": "The Croods",
+        "overview": "The prehistoric Croods family live in a particularly dangerous moment in time. Patriarch Grug, his mate Ugga, teenage daughter Eep, son Thunk, and feisty Gran gather food by day and huddle together in a cave at night. When a more evolved caveman named Guy arrives on the scene, Grug is distrustful, but it soon becomes apparent that Guy is correct about the impending destruction of their world.",
+        "poster_path": "/p7lJkqHlK01nr0zNacunUFI5Qxy.jpg",
+        "release_date": "2013-03-15",
+        "title": "The Croods",
+        "video": false,
+        "vote_average": 6.9,
+        "vote_count": 6419
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/xXWT0je8dTFFNBq6P2CeTZkPUu2.jpg",
+        "genre_ids": [
+          12,
+          28,
+          53,
+          9648
+        ],
+        "id": 2059,
+        "media_type": "movie",
+        "original_language": "en",
+        "original_title": "National Treasure",
+        "overview": "Modern treasure hunters, led by archaeologist Ben Gates, search for a chest of riches rumored to have been stashed away by George Washington, Thomas Jefferson and Benjamin Franklin during the Revolutionary War. The chest's whereabouts may lie in secret clues embedded in the Constitution and the Declaration of Independence, and Gates is in a race to find the gold before his enemies do.",
+        "poster_path": "/pxL6E4GBOPUG6CdkO9cUQN5VMwI.jpg",
+        "release_date": "2004-11-19",
+        "title": "National Treasure",
+        "video": false,
+        "vote_average": 6.6,
+        "vote_count": 5664
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/1dlXEyAxdysMCRvq9lgIr6Jds9H.jpg",
+        "genre_ids": [
+          53,
+          28,
+          14
+        ],
+        "id": 1250,
+        "media_type": "movie",
+        "original_language": "en",
+        "original_title": "Ghost Rider",
+        "overview": "In order to save his dying father, young stunt cyclist Johnny Blaze sells his soul to Mephistopheles and sadly parts from the pure-hearted Roxanne Simpson, the love of his life. Years later, Johnny's path crosses again with Roxanne, now a go-getting reporter, and also with Mephistopheles, who offers to release Johnny's soul if Johnny becomes the fabled, fiery 'Ghost Rider'.",
+        "poster_path": "/8LaVQiXBsnlo7MXCPK1nXTVARUZ.jpg",
+        "release_date": "2007-01-15",
+        "title": "Ghost Rider",
+        "video": false,
+        "vote_average": 5.5,
+        "vote_count": 5294
+      }
+    ],
+    "known_for_department": "Acting",
+    "name": "Nicolas Cage",
+    "popularity": 159.741,
+    "profile_path": "/ar33qcWbEgREn07ZpXv5Pbj8hbM.jpg"
+  };
+
+export { sampleMovie as default, samplePerson };
+
+
+
+
   
